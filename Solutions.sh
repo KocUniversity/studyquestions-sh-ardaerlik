f976469f7fa86975b9abb164c10eb271cd573281
Arda Erlik <71412093+ardaerlik@users.noreply.github.com>
2020-12-04 14:31:06 +0300

    Add files via upload

---
+++ Solutions.sh
@@ -0,0 +1,49 @@
+1. 
+echo "file1 is awesome" > file1
+echo $(cat file1) >> file1
+
+2.
+D)	10 10 77
+# line count, word count, and character count, respectively.
+
+3. 
+:8
+d3w
+G
+5p
+
+4.
+CTRL V
+L
+3e
+y
+0
+
+
+5. 
+for d in $(ls comp100_mt);
+do
+	echo $d
+done
+
+6.
+mkdir newdir 2> /dev/null
+
+
+10.
+  Press /
+  Type the search pattern.
+	Press Enter to perform the search.
+	Press n to find the next occurrence or N to find the previous occurrence.
+
+11.
+  Position the cursor where you want to begin cutting.
+
+  Press v to select characters, or uppercase V to select whole lines, or Ctrl-v to select rectangular blocks (use Ctrl-q if Ctrl-v is mapped to paste).
+
+  Move the cursor to the end of what you want to cut.
+  Press d to cut (or y to copy).
+
+  Move to where you would like to paste.
+
+  Press P to paste before the cursor, or p to paste after.
